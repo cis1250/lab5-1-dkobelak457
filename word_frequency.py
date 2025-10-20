14c62b8bf17ea22ccc275cb56d727e20fb828bf7
dkobelak457 <davidkobelak@gmail.com>
2025-10-20 12:21:20 -0400

    Implement word frequency calculation
    Added functions to process user input and calculate word frequencies.

--- word_frequency.py
+++ word_frequency.py
@@ -24,3 +24,39 @@ def is_sentence(text):
         return False
 
     return True
+
+def wordlist():
+    lower_sentence = user_sentence.lower()
+    cleaned_sentence = re.sub(r'[^\w\s]',' ', lower_sentence)
+    all_words = cleaned_sentence.split()
+    word = 0
+    for word in all_words:
+        if word in unique_words:
+        # If the word is already in the unique list, increment its count 
+            i = unique_words.index(word)
+            frequencies[i] += 1
+        else:
+        # If it's a new word, add it to the unique list and set its initial frequency to 1
+            unique_words.append(word)
+            frequencies.append(1)
+def results():
+    print("\n Word Frequency Results ")
+    # Only print results if words were actually found
+
+    #Took this from Gemeni AI
+    if unique_words:
+        for word, count in zip(unique_words, frequencies):
+            print(f"{word} : {count}")
+    else:
+        print("No words found ") 
+
+# validation loop
+user_sentence = input("Enter a sentence: ")
+while is_sentence(user_sentence) == False:
+    print("This does not meet the criteria for a sentence (Must start with a capital letter and end with .!?).")
+    user_sentence = input("Enter a valid sentence: ")
+
+unique_words = []
+frequencies = []
+wordlist()
+results()
