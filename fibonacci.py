351430a4a398502add678b8f696dd1bc74ee3a49
dkobelak457 <davidkobelak@gmail.com>
2025-10-20 13:16:13 -0400

    Refactor Fibonacci functions and simplify code
    Refactor Fibonacci sequence functions and remove comments.

--- fibonacci.py
+++ fibonacci.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python3
 
-# Fibonacci Sequence Exercise with functions
-# TODO: (Read detailed instructions in the Readme file)
-#!/usr/bin/env python3
-
-# Fibonacci Sequence Exercise
-# TODO: (Read detailed instructions in the Readme file)
-# Prompt the user for the number of terms.
-# Validate that the input is a positive integer.
-# Use a for loop to print the Fibonacci sequence up to that many terms.
-
-#While loop takes the user input and checks if it is a valid integer
 def get_input():
     while True:
         n = input("Enter how many Fibonacci Sequence integers do you want: ")
@@ -22,19 +11,21 @@ def get_input():
         else:
             print("Invalid, enter a whole number ")
 
-def generate_fibonacci(n):
+def gen_fibonacci(n):
     a = 0
     b = 1
-    #for loop takes a then calculates the next fib num using the t as a temp variable
-    #saves the old value of a, updates a and b to move the sequence forwards
     for i in range(n):
-        print(a, end=" ")
         t = a
         a = b
         b = t + b
+        yield t
 
-def print_fibonacci():
+def print_fib(sequence):
+    for num in sequence:
+        print(num, end=" ")
+    print()
+
+def fibonacci():
     n = get_input()
-    generate_fibonacci(n)
-
-print_fibonacci()
+    print_fib(gen_fibonacci(n))
+fibonacci()
